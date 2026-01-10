f56894a5289fce21a13f1691b1476a62be39a887
phantomstarelite <pratikkhumkar2004@gmail.com>
2026-01-10 23:06:26 +0530

    Add the practice file to repo

---
+++ src/practice/TwoS.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.HashMap;
+
+
+
+public class TwoS {
+
+    static int[] twoSum(int[] nums, int target){
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i <= nums.length; i++) {
+        int needed = target - nums[i];
+
+        if (map.containsKey(needed)) {
+            return new int[]{map.get(needed), i};
+        }
+        map.put(nums[i], i);
+    }
+    return new int[] {-1, -1};
+}
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] ans = twoSum(nums, target);
+
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
